811b1f70f4cd6bc425de89669ab358db7da767c4
tatyana2901 <tznatkova90@gmail.com>
2025-12-04 13:59:53 +0300

    добавлены методы генерации и валидации jwt токена

--- src/main/java/Internet_shop_NIC/Security/JwtService.java
+++ src/main/java/Internet_shop_NIC/Security/JwtService.java
@@ -1,30 +1,87 @@
 package Internet_shop_NIC.Security;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
+import Internet_shop_NIC.Entity.User;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.time.ZonedDateTime;
+import java.security.Key;
 import java.util.Date;
-
+import java.util.HashMap;
+import java.util.Map;
+//ЗДЕСЬ МЫ НЕСКОЛЬКО РАЗ ИЗВЛЕКАЕМ ДАННЫЕ ИЗ ТОКЕНА _СНАЧАЛОА ЧТОБЫ ПОЛУЧИТЬ А ПОТОМ ЧТОБЫ ПРОВЕРИТЬ -МОЖНО ИЗБЕЖАТЬ ДВОЙНОГО ИЗВЛЕЧЕНИЯ?
 @Service
 public class JwtService {
 
     @Value("${token.signing.key}")
     private String jwtSigningKey;
 
-    public String generateToken(String username) {
-        Date expirationDate = Date.from(ZonedDateTime.now().plusMinutes(60).toInstant());
+    public String createToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        if (userDetails instanceof User) {
+            User customUserDetails = (User) userDetails;
+            claims.put("id", customUserDetails.getId());
+            claims.put("last_name", customUserDetails.getLast_name());
+            claims.put("first_name", customUserDetails.getFirst_name());
+            claims.put("email", customUserDetails.getEmail());
+            claims.put("role", customUserDetails.getRole());
+        }
+        return generateToken(claims, userDetails);
+    }
 
-        return JWT.create()
-                .withSubject("User details")
-                .withClaim("username", username)
-                .withIssuedAt(new Date())
-                .withIssuer("NIC")
-                .withExpiresAt(expirationDate)
-                .sign(Algorithm.HMAC256(jwtSigningKey));
+    private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 100000 * 60 * 24))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSigningKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
 
+    // Проверка токена
+    public Claims extractClaims(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey()).build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractUserName(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+
+    public boolean validateToken(String token, String username) {
+        final String tokenUsername = extractUserName(token);
+        return (username.equals(tokenUsername) && !isTokenExpired(token));
+    }
+
+
+    public boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
 }
+
+
+
+
+
+
+
+
+
